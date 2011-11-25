2868fa09dd4bce80c0cf11b346a55ac51d022bef
jeroenpardon <jeroenpardon@gmail.com>
2011-11-25 17:57:15 +0100

    cleanup

--- TexturePacker.bat
+++ TexturePacker.bat
@@ -1,3 +1,6 @@
 @echo off
 
-START /B /WAIT TexturePacker -dupecheck -input media -output Textures.xbt
+START /B /WAIT TexturePacker -dupecheck -input D:\XBMC\addons\skin.refocus\media\ -output media\Textures.xbt
+
+echo Textures.xbt build complete - Scroll Up to check for errors.
+pause
