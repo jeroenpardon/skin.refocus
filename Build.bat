007a1b37175fc5bd09fd796e0908dbfef53471e8
jeroenpardon <czarina1>
2014-04-06 09:49:37 +0200

    Gotham version.
    First commit for the XBMC 13 "Gotham" compatible version of reFocus.

--- Build.bat
+++ Build.bat
@@ -6,35 +6,30 @@ md BUILD
 
 ECHO ------------------------------
 ECHO Building Skin Directory...
-xcopy "720p" "BUILD\skin.refocus.release\720p" /E /Q /I /Y
-xcopy "1080p" "BUILD\skin.refocus.release\1080p" /E /Q /I /Y
-xcopy "colors" "BUILD\skin.refocus.release\colors" /E /Q /I /Y
-xcopy "fonts" "BUILD\skin.refocus.release\fonts" /E /Q /I /Y
-xcopy "intro" "BUILD\skin.refocus.release\intro" /E /Q /I /Y
-xcopy "language" "BUILD\skin.refocus.release\language" /E /Q /I /Y
-xcopy "language\Dutch" "BUILD\skin.refocus.release\language\Dutch" /E /Q /I /Y
-xcopy "language\English" "BUILD\skin.refocus.release\language\English" /E /Q /I /Y
-xcopy "language\Finnish" "BUILD\skin.refocus.release\language\Finnish" /E /Q /I /Y
-xcopy "language\French" "BUILD\skin.refocus.release\language\French" /E /Q /I /Y
-xcopy "language\German" "BUILD\skin.refocus.release\language\German" /E /Q /I /Y
-xcopy "language\Italian" "BUILD\skin.refocus.release\language\Italian" /E /Q /I /Y
-xcopy "language\Korean" "BUILD\skin.refocus.release\language\Korean" /E /Q /I /Y
-xcopy "language\Norwegian" "BUILD\skin.refocus.release\language\Norwegian" /E /Q /I /Y
-xcopy "language\Polish" "BUILD\skin.refocus.release\language\Polish" /E /Q /I /Y
-xcopy "language\Romanian" "BUILD\skin.refocus.release\language\Romanian" /E /Q /I /Y
-xcopy "language\Russian" "BUILD\skin.refocus.release\language\Russian" /E /Q /I /Y
-xcopy "language\Spanish" "BUILD\skin.refocus.release\language\Spanish" /E /Q /I /Y
-xcopy "language\Swedish" "BUILD\skin.refocus.release\language\Swedish" /E /Q /I /Y
-xcopy "playlists" "BUILD\skin.refocus.release\playlists" /E /Q /I /Y
-xcopy "sounds" "BUILD\skin.refocus.release\sounds" /E /Q /I /Y
-xcopy "*.xml" "BUILD\skin.refocus.release\" /Q /I /Y
-xcopy "*.txt" "BUILD\skin.refocus.release\" /Q /I /Y
-xcopy "*.png" "BUILD\skin.refocus.release\" /Q /I /Y
+xcopy "720p" "BUILD\skin.refocus\720p" /E /Q /I /Y
+xcopy "colors" "BUILD\skin.refocus\colors" /E /Q /I /Y
+xcopy "backgrounds" "BUILD\skin.refocus\backgrounds" /E /Q /I /Y
+xcopy "fonts" "BUILD\skin.refocus\fonts" /E /Q /I /Y
+xcopy "language" "BUILD\skin.refocus\language" /E /Q /I /Y
+xcopy "language\Dutch" "BUILD\skin.refocus\language\Dutch" /E /Q /I /Y
+xcopy "language\English" "BUILD\skin.refocus\language\English" /E /Q /I /Y
+xcopy "language\Finnish" "BUILD\skin.refocus\language\Finnish" /E /Q /I /Y
+xcopy "language\French" "BUILD\skin.refocus\language\French" /E /Q /I /Y
+xcopy "language\Korean" "BUILD\skin.refocus\language\Korean" /E /Q /I /Y
+xcopy "language\Portuguese (Brazil)" "BUILD\skin.refocus\language\Portuguese (Brazil)" /E /Q /I /Y
+xcopy "language\Romanian" "BUILD\skin.refocus\language\Romanian" /E /Q /I /Y
+xcopy "playlists" "BUILD\skin.refocus\playlists" /E /Q /I /Y
+xcopy "sounds" "BUILD\skin.refocus\sounds" /E /Q /I /Y
+xcopy "*.xml" "BUILD\skin.refocus\" /Q /I /Y
+xcopy "*.txt" "BUILD\skin.refocus\" /Q /I /Y
+xcopy "*.png" "BUILD\skin.refocus\" /Q /I /Y
+xcopy "*.jpg" "BUILD\skin.refocus\" /Q /I /Y
+xcopy "*.md" "BUILD\skin.refocus\" /Q /I /Y
 
 ECHO ------------------------------
 ECHO Creating XBT File...
 START /B /WAIT TexturePacker.exe -input media\ -output media\Textures.xbt
-xcopy "media\*.xbt" "BUILD\skin.refocus.release\media\" /Q /I /Y
+xcopy "media\*.xbt" "BUILD\skin.refocus\media\" /Q /I /Y
 
 REM ECHO ------------------------------
 REM ECHO Cleaning Up...
@@ -42,7 +37,7 @@ REM del "media\*.xbt"
 
 REM ECHO ------------------------------
 REM ECHO Copying Image Files...
-REM xcopy "media\*.*" "BUILD\skin.refocus.release\media\" /E /Q /I /Y
+REM xcopy "media\*.*" "BUILD\skin.refocus\media\" /E /Q /I /Y
 
 echo Build Complete - Scroll Up to check for errors.
 pause
---
+++ Build.sh
@@ -0,0 +1,34 @@
+echo Creating reFocus Build Folder
+rm -R -f BUILD/
+mkdir BUILD
+mkdir BUILD/skin.refocus/
+
+echo Building Skin Directory...
+cp -R 720p BUILD/skin.refocus/720p
+cp -R backgrounds BUILD/skin.refocus/backgrounds
+cp -R colors BUILD/skin.refocus/colors
+cp -R fonts BUILD/skin.refocus/fonts
+cp -R language BUILD/skin.refocus/language
+cp -R language/Dutch BUILD/skin.refocus/language/Dutch
+cp -R language/English BUILD/skin.refocus/language/English
+cp -R language/Finnish BUILD/skin.refocus/language/Finnish
+cp -R language/French BUILD/skin.refocus/language/French
+cp -R language/Korean BUILD/skin.refocus/language/Korean
+cp -R language/Portuguese (Brazil) BUILD/skin.refocus/language/Portuguese (Brazil)
+cp -R language/Romanian BUILD/skin.refocus/language/Romanian
+cp -R playlists BUILD/skin.refocus/playlists
+cp -R sounds BUILD/skin.refocus/sounds
+cp *.xml BUILD/skin.refocus/
+cp *.txt BUILD/skin.refocus/
+cp *.png BUILD/skin.refocus/
+cp *.jpg BUILD/skin.refocus/
+cp *.md BUILD/skin.refocus/
+
+echo Creating XBT File...
+./TexturePacker -input media/ -output media/Textures.xbt
+mkdir BUILD/skin.refocus/media/
+cp media/*.xbt BUILD/skin.refocus/media/
+
+rm media/*.xbt
+
+echo Build Complete - Scroll Up to check for errors.
