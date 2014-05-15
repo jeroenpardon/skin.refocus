deefc16c65901ef0d9ea9fed7ed30c718d2f54a5
Jeroen <neeuuh@gmail.com>
2014-05-15 08:55:10 +0200

    updated build scripts

--- Build.sh
+++ Build.sh
@@ -20,9 +20,10 @@ cp -R language/Galician BUILD/skin.refocus/language/Galician
 cp -R language/Italian BUILD/skin.refocus/language/Italian
 cp -R language/Korean BUILD/skin.refocus/language/Korean
 cp -R language/Polish BUILD/skin.refocus/language/Polish
-cp -R language/Portuguese (Brazil) BUILD/skin.refocus/language/Portuguese (Brazil)
+cp -R language/Portuguese BUILD/skin.refocus/language/Portuguese
 cp -R language/Romanian BUILD/skin.refocus/language/Romanian
 cp -R language/Swedish BUILD/skin.refocus/language/Swedish
+cp -R language/Spanish BUILD/skin.refocus/language/Spanish
 cp -R playlists BUILD/skin.refocus/playlists
 cp -R sounds BUILD/skin.refocus/sounds
 cp *.xml BUILD/skin.refocus/
