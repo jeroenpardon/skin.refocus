381881996a716f4cf8563ef17633eb39c6516489
Jeroen <neeuuh@gmail.com>
2014-05-15 10:09:16 +0200

    new build script (thanks farfromrefug!)
    a new skin build script that can be run from anywhere and doesn't need the texture packer to be in the skin directory
    
    script by https://github.com/farfromrefug

--- Build.sh
+++ Build.sh
@@ -1,42 +1,105 @@
+#!/bin/bash 
+
+ScriptPath=$(cd "$(dirname "$0")"; pwd)
+
+OUTPUTDIR=build
+
+SkinPath=$ScriptPath
+TexturePackerPath=/Applications/TexturePacker
+OutputPath=$SkinPath/$OUTPUTDIR
+
+
+
+Usage()
+{
+cat <<-ENDOFMESSAGE
+
+$0 [OPTION] REQ1 REQ2
+
+options:
+
+    -o -output  Path to output to
+    -s -skin  Path to the skin to build
+    -p -path  Path to TexturePacker installation
+    -h --help   display this message
+
+ENDOFMESSAGE
+    exit 1
+}
+
+Die()
+{
+    echo "$*"
+    exit 1
+}
+
+GetOpts() {
+    branch="master"
+    argv=
+    while [ $# -gt 0 ]
+    do
+        opt=$1
+        shift
+        case ${opt} in
+            -p|--packer)
+                if [ $# -eq 0 -o "${1:0:1}" = "-" ]; then
+                    Die "The ${opt} option requires an argument."
+                fi
+                TexturePackerPath="$1"
+                shift
+                ;;
+            -s|--skin)
+                if [ $# -eq 0 -o "${1:0:1}" = "-" ]; then
+                    Die "The ${opt} option requires an argument."
+                fi
+                SkinPath="$1"
+                shift
+                ;;
+            -o|--output)
+                if [ $# -eq 0 -o "${1:0:1}" = "-" ]; then
+                    Die "The ${opt} option requires an argument."
+                fi
+                OutputPath="$1"
+                shift
+                ;;
+            -h|--help)
+                Usage;;
+            *)
+                if [ "${opt:0:1}" = "-" ]; then
+                    Die "${opt}: unknown option."
+                fi
+                argv+=${opt};;
+        esac
+    done
+}
+
+GetOpts $*
+
+SkinName=$(basename "${SkinPath}")
+echo "TexturePackerPath ${TexturePackerPath}"
+echo "SkinPath ${SkinPath}"
+echo "OutputPath ${OutputPath}"
+echo "ScriptPath ${ScriptPath}"
+echo "argv ${argv}"
+echo "Skin Name ${SkinName}"
+
 echo Creating reFocus Build Folder
-rm -R -f BUILD/
-mkdir BUILD
-mkdir BUILD/skin.refocus/
+rm -fr "$OutputPath"
+mkdir "$OutputPath"
+mkdir "$OutputPath/$SkinName"
 
 echo Building Skin Directory...
-cp -R 720p BUILD/skin.refocus/720p
-cp -R backgrounds BUILD/skin.refocus/backgrounds
-cp -R colors BUILD/skin.refocus/colors
-cp -R fonts BUILD/skin.refocus/fonts
-cp -R language BUILD/skin.refocus/language
-cp -R language/Bulgarian BUILD/skin.refocus/language/Bulgarian
-cp -R language/Czech BUILD/skin.refocus/language/Czech
-cp -R language/Danish BUILD/skin.refocus/language/Danish
-cp -R language/Dutch BUILD/skin.refocus/language/Dutch
-cp -R language/English BUILD/skin.refocus/language/English
-cp -R language/Finnish BUILD/skin.refocus/language/Finnish
-cp -R language/French BUILD/skin.refocus/language/French
-cp -R language/Galician BUILD/skin.refocus/language/Galician
-cp -R language/Italian BUILD/skin.refocus/language/Italian
-cp -R language/Korean BUILD/skin.refocus/language/Korean
-cp -R language/Polish BUILD/skin.refocus/language/Polish
-cp -R language/Portuguese BUILD/skin.refocus/language/Portuguese
-cp -R language/Romanian BUILD/skin.refocus/language/Romanian
-cp -R language/Swedish BUILD/skin.refocus/language/Swedish
-cp -R language/Spanish BUILD/skin.refocus/language/Spanish
-cp -R playlists BUILD/skin.refocus/playlists
-cp -R sounds BUILD/skin.refocus/sounds
-cp *.xml BUILD/skin.refocus/
-cp *.txt BUILD/skin.refocus/
-cp *.png BUILD/skin.refocus/
-cp *.jpg BUILD/skin.refocus/
-cp *.md BUILD/skin.refocus/
+
+for directory in $( ls "${SkinPath}" )
+do
+     if [ \( $directory != 'media' -a $directory != 'build' -a $directory != 'build.sh' \) ]; then
+         cp -R "${SkinPath}/$directory" "$OutputPath/$SkinName/$directory"
+     fi
+done
 
 echo Creating XBT File...
-./TexturePacker -dupecheck -input media/ -output media/Textures.xbt
-mkdir BUILD/skin.refocus/media/
-cp media/*.xbt BUILD/skin.refocus/media/
-
-rm media/*.xbt
-
+cd "${TexturePackerPath}"
+mkdir "$OutputPath/$SkinName/media"
+./TexturePacker -dupecheck -input "${SkinPath}/media/" -output "$OutputPath/$SkinName/media/Textures.xbt"
+cd -
 echo Build Complete - Scroll Up to check for errors.
