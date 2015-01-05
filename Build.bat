70aa7c6afe56a4b3fb0b6b37dd71839494bcba06
Jeroen <neeuuh@gmail.com>
2015-01-05 11:56:35 +0100

    Added Russian translation (thanks Olart)

--- Build.bat
+++ Build.bat
@@ -29,6 +29,7 @@ xcopy "language\Polish" "BUILD\skin.refocus\language\Polish" /E /Q /I /Y
 xcopy "language\Portuguese (Brazil)" "BUILD\skin.refocus\language\Portuguese (Brazil)" /E /Q /I /Y
 xcopy "language\Portuguese (Portugal)" "BUILD\skin.refocus\language\Portuguese (Portugal)" /E /Q /I /Y
 xcopy "language\Romanian" "BUILD\skin.refocus\language\Romanian" /E /Q /I /Y
+xcopy "language\Russian" "BUILD\skin.refocus\language\Russian" /E /Q /I /Y
 xcopy "language\Swedish" "BUILD\skin.refocus\language\Swedish" /E /Q /I /Y
 xcopy "language\Spanish" "BUILD\skin.refocus\language\Spanish" /E /Q /I /Y
 xcopy "playlists" "BUILD\skin.refocus\playlists" /E /Q /I /Y
